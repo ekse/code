6af5854c8b3ea30dd18c4f70505885bd9017de8a
ekse <ekse@arch1.(none)>
2009-04-23 06:22:24 -0400

    added pidfork script

--- pidfork.sh
+++ pidfork.sh
@@ -1,3 +1,12 @@
+##################################
+# Sebastien Duquette - 2009       #
+#                                 #
+# pidfork                         #
+# spawns the command in argument  #
+# and echoes the process id.      #
+#		                  #
+##################################
+
 if [ $# -lt 1 ]; then
 	echo -n "usage: $0 <command>";
 	exit
