9db8a30156c247faa1d28ab27d12cf48eea30929
Sébastien Duquette <ekse.0x@gmail.com>
2015-07-13 21:04:54 -0400

    exercism anagram : slight reformating

--- labs/exercism/cpp/anagram/anagram.cpp
+++ labs/exercism/cpp/anagram/anagram.cpp
@@ -1,15 +1,14 @@
 #include <algorithm>
 #include <vector>
 #include <string>
-#include <boost/algorithm/string.hpp>  
+#include <boost/algorithm/string.hpp>
 
 #include "anagram.h"
 
 using namespace std;
 
-anagram::anagram(string word) {
-    word_ = word;
-    word_lowercase_ = boost::algorithm::to_lower_copy(word);
+anagram::anagram(string word)
+    : word_(word), word_lowercase_(boost::algorithm::to_lower_copy(word)) {
     word_lowercase_sorted_ = word_lowercase_;
     sort(word_lowercase_sorted_.begin(), word_lowercase_sorted_.end());
 }
@@ -22,7 +21,7 @@ vector<string> anagram::matches(const vector<string>& words) {
         if (word.length() != word_.length()) {
             continue;
         }
-        
+
         auto word_lowercase = boost::algorithm::to_lower_copy(word);
 
         // a word is not an anagram of itself
@@ -31,7 +30,7 @@ vector<string> anagram::matches(const vector<string>& words) {
         }
 
         // compare words in lowercase and alphabetical order
-        auto word_lowercase_sorted = word_lowercase; 
+        auto word_lowercase_sorted = word_lowercase;
         sort(word_lowercase_sorted.begin(), word_lowercase_sorted.end());
 
         if (word_lowercase_sorted.compare(word_lowercase_sorted_) == 0) {
--- labs/exercism/cpp/anagram/anagram.h
+++ labs/exercism/cpp/anagram/anagram.h
@@ -3,14 +3,13 @@
 
 using namespace std;
 
-
 class anagram {
    public:
     anagram(const string word);
     vector<string> matches(const vector<string>& words);
+
    private:
     string word_;
     string word_lowercase_;
     string word_lowercase_sorted_;
-
 };
