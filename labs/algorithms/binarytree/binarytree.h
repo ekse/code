ea9a7543bd4207e343d6b90cd051d2303825618b
Sébastien Duquette <ekse.0x@gmail.com>
2015-07-19 21:57:39 -0400

    binarytree: big changes to the implementation.
    - Removed BinaryTree class, trees are declared directly as MyNode instead.
    - Tree is now balanced at insertion.
    - Switched to CMake for building with CLion.

--- labs/algorithms/binarytree/binarytree.h
+++ labs/algorithms/binarytree/binarytree.h
@@ -1,59 +1,101 @@
 #include <cstddef>
 #pragma once
 
-/* This class implements an AVL tree.
+/* TODO:
+ - make BinaryTree an abstract class and AVLTree a subclass.
+ - implement Traverse() iterator
+
+ * This class implements an AVL tree.
  *
+ *The balance_factor of a node is calculated as height(left_subtree) -
+ *height(right_subtree). For a balanced tree the balance_factor of all nodes
+ *needs to be in interval (-1,+1). When this range is exceeded, a rotation needs
+ *to be done on the subtree.
  */
 
 template <typename T>
-class BinaryTree;
-
-template <typename T>
 class MyNode {
-    friend class BinaryTree<T>;
-
    public:
     MyNode<T>(T value);
-
-   private:
-    T value_;
-    MyNode<T>* left_;
-    MyNode<T>* right_;
-};
-
-template <typename T>
-MyNode<T>::MyNode(T value)
-    : left_(nullptr), right_(nullptr) {
-    value_ = value;
-}
-
-// BinaryTree
-template <typename T>
-class BinaryTree {
-   public:
-    BinaryTree<T>();
     void Insert(T value);
     bool Contains(T value);
-    bool IsEmpty() { return (root_ == nullptr); };
+    MyNode<T>* RotateLeft();
+    MyNode<T>* RotateRight();
     T Min();
     T Max();
     // void traverse(); -- not implemented yet
 
+    static int Height(MyNode<T>* p) { return p ? p->height_:0;};
+    static int BalanceFactor(MyNode<T>* p) { return Height(p->right_) - Height(p->left_); }
+
    private:
-    MyNode<T>* root_;
+    T value_;
+    MyNode<T>* parent_;
+    MyNode<T>* left_;
+    MyNode<T>* right_;
+    int height_;
+
+    void FixHeight();
+    MyNode<T>* Balance();
 };
 
 template <typename T>
-BinaryTree<T>::BinaryTree()
-    : root_(nullptr) {}
+MyNode<T>::MyNode(T value)
+    : left_(nullptr), right_(nullptr), parent_(nullptr), height_(1) {
+    value_ = value;
+}
 
 template <typename T>
-bool BinaryTree<T>::Contains(T value) {
-    if (root_ == nullptr) {
-        return false;
+MyNode<T>* MyNode<T>::RotateLeft() {
+    MyNode<T>* p = this;
+    MyNode<T>* q = p->right_;
+    p->right_ = q->left_;
+    q->left_ = p;
+    p->FixHeight();
+    q->FixHeight();
+    return q;
+}
+
+template <typename T>
+MyNode<T>* MyNode<T>::RotateRight() {
+    MyNode<T>* p = this;
+    MyNode<T>* q = p->left_;
+    p->left_ = q->right_;
+    q->right_ = p;
+    p->FixHeight();
+    q->FixHeight();
+    return q;
+}
+
+template <typename T>
+void MyNode<T>::FixHeight() {
+    int height_left = Height(left_);
+    int height_right = Height(right_);
+    height_ = (height_left > height_right ? height_left : height_right) + 1;
+}
+
+template<typename T>
+MyNode<T> *MyNode<T>::Balance() {
+    MyNode<T> *p = this;
+    FixHeight();
+    if (BalanceFactor(p) == 2) {
+        if (BalanceFactor(right_) < 0)
+            right_ = p->right_->RotateRight();
+        return (p->RotateLeft());
     }
 
-    auto cur_node = root_;
+    if (BalanceFactor(p) == -2) {
+        if (BalanceFactor(left_) > 0)
+            left_ = p->left_->RotateLeft();
+        return (p->RotateRight());
+    }
+    return this; // no balance needed
+}
+
+template<typename T>
+bool MyNode<T>::Contains(T value) {
+
+    auto cur_node = this;
 
     while (true) {
         if (value == cur_node->value_) {
@@ -73,41 +115,37 @@ bool BinaryTree<T>::Contains(T value) {
 }
 
 template <typename T>
-void BinaryTree<T>::Insert(T value) {
+void MyNode<T>::Insert(T value) {
     MyNode<T>* node = new MyNode<T>(value);
 
-    if (root_ == nullptr) {
-        root_ = node;
-        return;
-    }
-
-    auto cur_node = root_;
+    auto cur_node = this;
 
     while (true) {
         if (value < cur_node->value_) {
             if (cur_node->left_ == nullptr) {
                 cur_node->left_ = node;
-                return;
+                node->parent_ = cur_node;
+                break;
             }
             cur_node = cur_node->left_;
         } else {
             if (cur_node->right_ == nullptr) {
                 cur_node->right_ = node;
-                return;
+                node->parent_ = cur_node;
+                break;
             }
             cur_node = cur_node->right_;
         }
     }
+
+    Balance();
 }
 
 // default min value is 0
 template <typename T>
-T BinaryTree<T>::Min() {
-    if (root_ == nullptr) {
-        return 0;
-    }
+T MyNode<T>::Min() {
 
-    auto cur_node = root_;
+    auto cur_node = this;
 
     while (true) {
         if (cur_node->left_ == nullptr) {
@@ -120,12 +158,8 @@ T BinaryTree<T>::Min() {
 
 // default max value is 0
 template <typename T>
-T BinaryTree<T>::Max() {
-    if (root_ == nullptr) {
-        return 0;
-    }
-
-    auto cur_node = root_;
+T MyNode<T>::Max() {
+    auto cur_node = this;
 
     while (true) {
         if (cur_node->right_ == nullptr) {
--- labs/algorithms/binarytree/binarytree_unittest.cc
+++ labs/algorithms/binarytree/binarytree_unittest.cc
@@ -3,44 +3,35 @@
 
 namespace {
 
-TEST(BinaryTree, IsEmpty) {
-    auto tree = BinaryTree<int>();
-    EXPECT_EQ(tree.IsEmpty(), true);
+    TEST(BinaryTree, Contains) {
+        auto tree = MyNode<int>(0);
+        EXPECT_EQ(tree.Contains(1), false);
 
-    tree.Insert(1);
-    EXPECT_EQ(tree.IsEmpty(), false);
-}
+        tree.Insert(1);
+        EXPECT_EQ(tree.Contains(0), true);
+        EXPECT_EQ(tree.Contains(1), true);
+        EXPECT_EQ(tree.Contains(2), false);
+        EXPECT_EQ(tree.Contains(3), false);
 
-TEST(BinaryTree, Contains) {
-    auto tree = BinaryTree<int>();
-    EXPECT_EQ(tree.Contains(1), false);
+        tree.Insert(2);
+        EXPECT_EQ(tree.Contains(0), true);
+        EXPECT_EQ(tree.Contains(1), true);
+        EXPECT_EQ(tree.Contains(2), true);
+        EXPECT_EQ(tree.Contains(3), false);
+    }
 
-    tree.Insert(1);
-    EXPECT_EQ(tree.Contains(0), false);
-    EXPECT_EQ(tree.Contains(1), true);
-    EXPECT_EQ(tree.Contains(2), false);
+    TEST(BinaryTree, Insert) {
+        auto tree = MyNode<int>(1);
+        tree.Insert(2);
+        tree.Insert(6);
+        tree.Insert(10);
+        tree.Insert(8);
 
-    tree.Insert(2);
-    EXPECT_EQ(tree.Contains(0), false);
-    EXPECT_EQ(tree.Contains(1), true);
-    EXPECT_EQ(tree.Contains(2), true);
-}
-
-TEST(BinaryTree, Insert) {
-    auto tree = BinaryTree<int>();
-
-    tree.Insert(1);
-    tree.Insert(2);
-    tree.Insert(6);
-    tree.Insert(8);
-    tree.Insert(10);
-
-    EXPECT_EQ(tree.Min(), 1);
-    EXPECT_EQ(tree.Max(), 10);
-}
+        EXPECT_EQ(tree.Min(), 1);
+        EXPECT_EQ(tree.Max(), 10);
+    }
 
 }  // namespace
-
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
