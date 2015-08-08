f82ce95a9d18e1e9968995542fa71673fb46c3e2
Sébastien Duquette <ekse.0x@gmail.com>
2015-08-08 17:04:33 -0400

    exercism rust : 2nd version of word-count
    - use to_lowercase() from std
    - use .entry() and .or_insert() to add or update in HashMap

--- labs/exercism/rust/word-count/src/lib.rs
+++ labs/exercism/rust/word-count/src/lib.rs
@@ -1,35 +1,15 @@
 use std::collections::HashMap;
 
-fn lowercase(s : &str) -> String {
-    
-    let mut out = String::with_capacity(s.len());
-    for c in s.chars() {
-        for lower in c.to_lowercase() {
-            out.push(lower);
-        }
-    }
-    
-    out
-    
-}
-
 pub fn word_count(text : &str) -> HashMap<String, u32> {
-
     let mut words : HashMap<String, u32> = HashMap::new();
     
     for word in text.split(|c : char| !c.is_alphanumeric()) {
-
-        if word.len() == 0 {
+        if word.is_empty() {
             continue
         }
-
-        let lowercase_word = lowercase(word);
-
-        if words.contains_key(&lowercase_word) {
-           *words.get_mut(&lowercase_word).unwrap() += 1; 
-        } else {
-            words.insert(lowercase_word, 1);
-        }
+        
+        let lowercase_word = word.to_lowercase();
+        *words.entry(lowercase_word).or_insert(0) += 1
     }
     
     words
