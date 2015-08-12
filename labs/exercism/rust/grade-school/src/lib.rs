9808ce6ad5c33e455c28188c334276920fd96050
Sébastien Duquette <ekse.0x@gmail.com>
2015-08-11 23:27:24 -0400

    exercism rust : first version of grade-school

---
+++ labs/exercism/rust/grade-school/src/lib.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+
+pub struct School {
+    years : HashMap<u32, Vec<String>>,
+}
+
+impl School {
+    pub fn new() -> School {
+        School{years : HashMap::new()}
+    }
+
+    pub fn add(&mut self, grade : u32, name : &'static str) {
+        let mut names = self.years.entry(grade).or_insert(Vec::new());
+        names.push(name.to_string());
+        names.sort();
+    }
+
+    pub fn grades(&self) -> Vec<u32> {
+        let mut out = Vec::new();
+        for k in self.years.keys() {
+            out.push(*k);
+        }
+        out.sort();
+        out
+    }
+
+    pub fn grade(&self, year : u32) -> Option<&Vec<String>> {
+        self.years.get(&year)
+    }
+}
