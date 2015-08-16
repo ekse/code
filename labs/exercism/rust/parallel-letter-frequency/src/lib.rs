dd769e76dee0960f96948e4f3e68cb04bfa6c431
Sébastien Duquette <ekse.0x@gmail.com>
2015-08-15 22:34:42 -0400

    exercism rust : letter frequency, added a function to merge the histograms

--- labs/exercism/rust/parallel-letter-frequency/src/lib.rs
+++ labs/exercism/rust/parallel-letter-frequency/src/lib.rs
@@ -1,13 +1,30 @@
 use std::collections::HashMap;
+use std::thread;
 
-pub fn frequency(texts : &[&str], works : usize) -> HashMap<char, usize> {
+pub type Histogram = HashMap<char, usize>;
+
+fn count_letters(text : &str) -> Histogram {
     let mut letters = HashMap::new();
-    for text in texts {
-        for c in text.chars() {
-            if c.is_alphabetic() {
-                *letters.entry(c.to_lowercase().next().unwrap()).or_insert(0) += 1;
-            }
+    for c in text.chars() {
+        if c.is_alphabetic() {
+            *letters.entry(c.to_lowercase().next().unwrap()).or_insert(0) += 1;
         }
+    }
+    letters
+}
+
+fn merge_histograms(a : &mut Histogram, b : &Histogram ) {
+    for (k,v) in b.iter() {
+        *a.entry(*k).or_insert(0) += *v;
+    }
+}
+
+pub fn frequency(texts : &[&str], works : usize) -> Histogram {
+    let mut letters = HashMap::new();
+    
+    for text in texts {
+        let text_letters = count_letters(text);
+        merge_histograms(&mut letters, &text_letters);
     }
     letters
 }
