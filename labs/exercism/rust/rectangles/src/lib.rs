17871d5448a4cf77f63fe48e99f940d87dfefa84
Sébastien Duquette <ekse.0x@gmail.com>
2015-09-01 23:32:52 -0400

    exercism rust : rectangles, minor edits

--- labs/exercism/rust/rectangles/src/lib.rs
+++ labs/exercism/rust/rectangles/src/lib.rs
@@ -5,6 +5,8 @@ enum Direction {
     Right,
 }
 
+// I use signed values for Point so that negative values indicate
+// that a point is outside the grid. It simplifies bound-checking.
 #[derive(Debug, Clone, Copy)]
 struct Point {
     x : isize,
@@ -36,11 +38,8 @@ impl Grid {
         
         let nb_rows = lines.len();
         let nb_columns = lines[0].len();
-        println!("columns : {} rows : {}", nb_rows, nb_columns);
         
         let mut grid : Vec<char> = Vec::with_capacity(nb_rows * nb_columns);
-        // this could probably be rewritten without for-loops
-        // do the assert line lengths separately
         for i in 0..nb_rows {
             if lines[i].len() != nb_columns {
                 return None;
@@ -64,9 +63,12 @@ impl Grid {
     }
 
     fn is_inbounds(&self, p : &Point) -> bool {
-        (p.x >= 0) & (p.y >= 0) && ((p.y as usize) * self.columns + (p.x as usize)) < self.grid.len()
+        (p.x >= 0) && (p.y >= 0) && 
+        ((p.y as usize) * self.columns + (p.x as usize)) < self.grid.len()
     }
 
+    // returns true if we can move 1 step direction specified starting from the
+    // point p.
     fn can_go_from(&self, direction : Direction, p: &Point) -> bool {
         match direction {
             Direction::Left  => { 
@@ -99,6 +101,9 @@ impl Grid {
     }
 }
 
+
+// return the list of rectangles that have 'point' in the the top-left corner
+// and that are to the right side of 'point'.
 fn rectangles_from_point(grid : &Grid, point : &Point) -> Vec<Rectangle> {
     let mut candidates : Vec<(Direction, Rectangle)> = Vec::new();
     let mut rectangles : Vec<Rectangle> = Vec::new();
@@ -168,6 +173,34 @@ fn rectangles_from_point(grid : &Grid, point : &Point) -> Vec<Rectangle> {
 
 }
 
+/*  We scan each line of the grid for a start corner. The scanning is
+    done left to right starting from the top left corner of the grid. 
+    
+    When we find a corner, we collect the rectangles that have this corner
+    in the top left position (a) by scanning in the following order :
+    Right, Down, Left, Up. Always scanning in this direction makes sure
+    we do not count doubles.
+
+    Let's take the following grid as an example.
+    
+    +-----+----+
+    |     |    |
+    +-----+----+
+
+    The first pass starting at the point a will find 2 rectangles.
+
+    a-----b----b
+    |     |    |
+    d-----c----c
+
+    The second pass will find the 3rd rectangle.
+
+          a----b
+          |    |
+          d----c
+
+
+*/
 pub fn count(lines : &Vec<&str>) -> usize {
 
     let grid = match Grid::from_string(lines) {
@@ -175,17 +208,16 @@ pub fn count(lines : &Vec<&str>) -> usize {
         None => return 0,
     };
     
-    grid.print();
+    //grid.print();
     let mut nb_rectangles = 0;
 
-    // scan the grid left to right from the top left until we encounter a corner (+)
     for i in 0..grid.columns {
         for j in 0..grid.rows {
-            if grid.at_coords(i,j) == '+' {
-                println!("corner at ({},{})", i, j);
+            if grid.at_coords(i, j) == '+' {
+                // we have a corner, next we check if we can at least go one step
+                // to the right.
                 let point = Point{x : (i as isize), y : (j as isize)};
                 if grid.can_go_from(Direction::Right, &point) {
-                    println!("potential starting point : {:?}", point);
                     nb_rectangles += rectangles_from_point(&grid, &point).len();
                 }
             }
