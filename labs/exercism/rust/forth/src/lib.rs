f9252f3cb775addd80d873c85965c0bf6b6e1155
Sébastien Duquette <ekse.0x@gmail.com>
2015-09-16 22:03:39 -0400

    exercism/rust: first working version of forth exercise
    - word definition does not accept characters other than letters

--- labs/exercism/rust/forth/src/lib.rs
+++ labs/exercism/rust/forth/src/lib.rs
@@ -1,4 +1,5 @@
 use std::iter::Iterator;
+use std::collections::HashMap;
 
 pub type Value = i32;
 pub type ForthResult = Result<(), Error>;
@@ -11,13 +12,31 @@ pub enum Error {
     InvalidWord,
 }
 
+pub fn is_word(s : &String) -> bool {
+    return s.len() > 0 && s.chars().all(|c| c.is_alphabetic())
+
+}
+
+// TODO: finish this function
+/*
+pub fn is_number(s : String) -> bool {
+    if s.len() == 0 {
+        return false;
+    }
+    return true;
+}
+*/
+
+
+
 pub struct Forth {
     stack: Vec<i32>,
+    words: HashMap<String, Vec<String>>,
 }
 
 impl Forth {
     pub fn new() -> Forth {
-        Forth { stack: vec![] }
+        Forth { stack: vec![], words : HashMap::new() }
 
     }
 
@@ -34,15 +53,31 @@ impl Forth {
     pub fn eval(&mut self, s: &'static str) -> ForthResult {
         let mut input = s.to_uppercase();
         input = input.chars().map(|c| {
-            if c.is_alphanumeric() || "+-*/".find(c) != None {
+            if c.is_alphanumeric() || "+-*/:;".find(c) != None {
                 c
             } else {
                 ' '
             }
         }).collect();
 
-
+        let mut tokens : Vec<String> = vec![];
         for w in input.split_whitespace() {
+            tokens.push(w.to_string());
+        }
+        self.eval_tokens(&tokens)
+    }
+    
+    pub fn eval_tokens(&mut self, tokens : &Vec<String>) -> ForthResult {
+
+        let mut it = tokens.iter(); 
+        loop {
+            // TODO: change this block to an if-let
+            let t = it.next();
+            if t == None {
+               return Ok(());
+            } 
+             
+            let mut w = t.unwrap();
             println!("w : {}", w);
 
             if w == "+" {
@@ -86,6 +121,13 @@ impl Forth {
 
                 self.stack.push(a / b);
 
+            } else if self.words.contains_key(&w.to_string()) {
+                let tokens = &self.words.get(&w.to_string()).unwrap().clone();
+                match self.eval_tokens(tokens) {
+                    Ok(_) => (),
+                    Err(e) => return Err(e),
+                }
+
             } else if w == "DUP" {
                 if self.stack.len() < 1 {
                     return Err(Error::StackUnderflow);
@@ -110,7 +152,6 @@ impl Forth {
                 let b = self.stack.pop().unwrap();
                 let a = self.stack.pop().unwrap();
 
-
                 self.stack.push(b);
                 self.stack.push(a);
 
@@ -122,17 +163,56 @@ impl Forth {
                 let b = self.stack.pop().unwrap();
                 let a = self.stack.pop().unwrap();
 
-
                 self.stack.push(a);
                 self.stack.push(b);
                 self.stack.push(a);
-
-            } else {
+            
+            // TODO: replace with is_number method and allow - or + only at
+            // the beginning.
+            } else if w.chars().all(|c| c.is_numeric() || "-+".find(c) != None) {
                 let v = w.parse::<i32>().unwrap();
                 self.stack.push(v);
+           
+            } else if w == ":" {
+                // handle new word definition
+               
+                // read the word
+                let t = it.next();
+                if t == None {
+                    return Err(Error::InvalidWord);  
+                }
+                
+                let word : String = t.unwrap().to_string();
+                if !is_word(&word) {
+                    return Err(Error::InvalidWord);  
+                }
+                
+                let mut definition : Vec<String> = vec![];
+                
+               loop {
+                    let t = it.next();
+                    if t == None {
+                        return Err(Error::InvalidWord);  
+                    }
+                    
+                    w = t.unwrap();
+
+                    if w == ";" {
+                        break;
+                    }
+                    
+                    definition.push(w.to_string());
+                }
+
+                if definition.len() == 0 {
+                    return Err(Error::InvalidWord);  
+                }
+
+                self.words.insert(word, definition);
+
+            } else {
+                return Err(Error::UnknownWord);        
             }
         }
-
-        Ok(())
     }
 }
